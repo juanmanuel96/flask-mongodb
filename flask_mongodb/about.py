ba2f268815f71383a8c2864883c04df3c3388343
juanmanuel96 <juanmapr913@gmail.com>
2022-07-27 21:22:29 -0400

    Updated version number

--- flask_mongodb/about.py
+++ flask_mongodb/about.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme_file:
     __long_description__ = readme_file.read()
 
 __url__ = 'https://github.com/juanmanuel96/flask-mongodb'
-__version_info__ = ('1', '4', '0')
+__version_info__ = ('1', '4', '1')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Juan Vazquez'
 __author_email__ = 'juanmapr913@gmail.com'
