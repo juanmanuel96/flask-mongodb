251819dcdff3e8c12fcf2635e71337c807f06306
juanmanuel96 <juanmapr913@gmail.com>
2022-08-02 20:16:32 -0400

    bugfix during package install

--- flask_mongodb/utils/version.py
+++ flask_mongodb/utils/version.py
@@ -7,12 +7,11 @@ def get_version(version_info: tuple) -> str:
     version_number: tuple[int] = version_info[:3]  # First three numbers are the MAJOR, MINOR, and PATCH
     version_state: str = version_info[3]  # Package state
     state_number: int = version_info[4]  # State number
-    version = '.'.join(version_number)
+    version = '.'.join([str(v) for v in version_number])
     
-    if version_state not in VALID_STATES:
+    if version_state.upper() not in VALID_STATES:
         # TODO Need specific Exception for this
         raise Exception('Not a valid package state')
-    
     if version_state.upper() == FINAL:
         return version
     if RELEASE_CANDIDATE in version_state.upper():
