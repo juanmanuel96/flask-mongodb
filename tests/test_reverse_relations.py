5f96c30ec3249b7ccab267644e54d90065f411a7
juanmanuel96 <juanmapr913@gmail.com>
2022-09-11 23:48:46 -0400

    more tests for reference and reverse relations

--- tests/test_reverse_relations.py
+++ tests/test_reverse_relations.py
@@ -2,12 +2,13 @@ import pytest
 from flask import Flask
 
 from flask_mongodb.core.mongo import MongoDB
+from flask_mongodb.models.fields import ReferenceIdField
 from tests.fixtures import BaseAppSetup
 from tests.model_for_tests.reference.models import CarCompany, CarModel
 from tests.utils import DB_NAME, MAIN
 
 
-class TestReverseRelations(BaseAppSetup):
+class TestReferenceAndReverseRelations(BaseAppSetup):
     APP_CONFIG = {
         'TESTING': True,
         'DATABASE': {
@@ -36,7 +37,30 @@ class TestReverseRelations(BaseAppSetup):
         model['year'] = 2019
         model.manager.insert_one(model.data(include_reference=False))
     
-    def test_models_relation_attribute(self, application: Flask):
-        model = application.mongo.get_collection(CarCompany)
+    @pytest.fixture(scope='class')
+    def car_company(self, mongo: MongoDB) -> CarCompany:
+        model = mongo.get_collection(CarCompany)
         car_company = model.manager.find_one(company_name='GM')
-        assert hasattr(car_company, 'car_models')
+        return car_company
+    
+    @pytest.fixture(scope='class')
+    def car_model(self, mongo: MongoDB) -> CarModel:
+        model = mongo.get_collection(CarModel)
+        car_model = model.manager.find_one(make='Chevrolet')
+        return car_model
+    
+    # TESTS
+    def test_reference(self, car_model: CarModel):
+        company = car_model['company']
+        assert isinstance(company, CarCompany)
+    
+    def test_reference_field(self, car_model: CarModel):
+        company = car_model.company
+        assert isinstance(company, ReferenceIdField)
+    
+    def test_models_relation_attribute(self, car_company: CarCompany):
+        assert hasattr(car_company,  CarModel.company.related_name)
+    
+    def test_relation_finds_correct_data(self, car_company: CarCompany):
+        car_model = car_company.car_models.find().first()
+        assert car_model['car_model'] == 'Camaro'
