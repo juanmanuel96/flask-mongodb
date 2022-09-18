9a41bc786a5acdeed2bdeedafd4265f021821a6e
juanmanuel96 <juanmapr913@gmail.com>
2022-09-18 16:05:07 -0400

    fixed fixture name issue

--- tests/test_references/test_reverse_relations.py
+++ tests/test_references/test_reverse_relations.py
@@ -12,34 +12,34 @@ class TestReferenceAndReverseRelations(TestReferencesSetUp):
     MODELS = ['tests.model_for_tests.reference']
     
     # TESTS
-    def test_models_relation_attribute(self, car_company: CarCompany):
-        assert hasattr(car_company,  CarModel.company.related_name)
+    def test_models_relation_attribute(self, gm_company: CarCompany):
+        assert hasattr(gm_company,  CarModel.company.related_name)
     
-    def test_relation_finds_correct_data(self, car_company: CarCompany):
-        car_model = car_company.car_models.find().first()
+    def test_relation_finds_correct_data(self, gm_company: CarCompany):
+        car_model = gm_company.car_models.find().first()
         assert car_model['car_model'] == 'Camaro'
     
-    def test_insert_not_allowed(self, car_company: CarCompany):
+    def test_insert_not_allowed(self, gm_company: CarCompany):
         with pytest.raises(exceptions.OperationNotAllowed):
-            car_company.car_models.insert_one({})
+            gm_company.car_models.insert_one({})
     
-    def test_insert_many_not_allowed(self, car_company: CarCompany):
+    def test_insert_many_not_allowed(self, gm_company: CarCompany):
         with pytest.raises(exceptions.OperationNotAllowed):
-            car_company.car_models.insert_many({})
+            gm_company.car_models.insert_many({})
     
-    def test_update_one_not_allowed(self, car_company: CarCompany):
+    def test_update_one_not_allowed(self, gm_company: CarCompany):
         with pytest.raises(exceptions.OperationNotAllowed):
-            car_company.car_models.update_one({}, {})
+            gm_company.car_models.update_one({}, {})
     
-    def test_delete_one_not_allowed(self, car_company: CarCompany):
+    def test_delete_one_not_allowed(self, gm_company: CarCompany):
         with pytest.raises(exceptions.OperationNotAllowed):
-            car_company.car_models.delete_one({})
+            gm_company.car_models.delete_one({})
     
-    def test_add_new_car_model(self, car_company: CarCompany, mongo: MongoDB):
-        total_company_models = car_company.car_models.all().count()  # Should be 1
+    def test_add_new_car_model(self, gm_company: CarCompany, mongo: MongoDB):
+        total_company_models = gm_company.car_models.all().count()  # Should be 1
         car = mongo.get_collection(CarModel)
-        car.set_model_data({'company': car_company.pk, 'make': 'Chevrolet', 'car_model': 'Corvette',
+        car.set_model_data({'company': gm_company.pk, 'make': 'Chevrolet', 'car_model': 'Corvette',
                               'color': 'red', 'year': 2022})
         car.manager.insert_one(car.data(include_reference=False))
-        new_total = car_company.car_models.all().count()  # Should be 2
+        new_total = gm_company.car_models.all().count()  # Should be 2
         assert new_total > total_company_models
