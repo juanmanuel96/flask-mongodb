97ddb239a0b7d4dcdc8e8265399d821b790e12d7
juanmanuel96 <juanmapr913@gmail.com>
2022-10-24 23:37:01 -0400

    fixed model view and updated version number

--- flask_mongodb/views.py
+++ flask_mongodb/views.py
@@ -1,20 +1,22 @@
 import typing as t
-from flask import current_app
 from flask.views import MethodView
 from flask_mongodb.core.exceptions import MissingViewModelException
 
 from flask_mongodb.core.mongo import CollectionModel
-from flask_mongodb.core.mixins import ModelMixin
 
 
-class ModelView(MethodView, ModelMixin):
+class ModelView(MethodView):
     view_model: t.Type[CollectionModel] = None
 
     def __init__(self):
         if self.view_model:
             assert issubclass(self.view_model, CollectionModel)
-            self._model = current_app.mongo.get_collection(self.view_model)
+            self._model = self.view_model()
         else:
             raise MissingViewModelException()
         super().__init__()
+    
+    @property
+    def model(self):
+        return self._model
 
